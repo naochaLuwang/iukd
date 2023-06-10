1f0a2e87d14ce17ed7f07574c1b69a35bd2f9355
Somojit Arambam <somoarambam123@gmail.com>
2023-06-10 10:40:53 +0530

    Embedded page

--- app/outreach_opds/page.tsx
+++ app/outreach_opds/page.tsx
@@ -13,22 +13,33 @@ const OutreachPage = async () => {
       <Banner title="Outreach OPDs" sublink="Outreach OPDs" />
       <div className="grid w-full grid-cols-1 gap-10 px-6 py-10 mx-auto lg:py-20 lg:px-0 lg:grid-cols-4 max-w-7xl">
         {doctors.map((doctor) => (
-          <div key={doctor.id} className="w-full h-[30rem] bg-white shadow-md">
+          <div key={doctor.id} className="w-full h-[32rem] bg-white shadow-md">
             <div className="relative w-full h-56 ">
-              <Image
-                src={doctor.profileUrl}
-                alt={doctor.firstName}
-                fill
-                style={{ objectFit: "fill" }}
-              />
+              {doctor.profileUrl === "" ? (
+                <Image
+                  src="/docplaceholder.jpeg"
+                  alt={doctor.firstName}
+                  fill
+                  style={{ objectFit: "fill" }}
+                />
+              ) : (
+                <Image
+                  src={doctor.profileUrl}
+                  alt={doctor.firstName}
+                  fill
+                  style={{ objectFit: "fill" }}
+                />
+              )}
             </div>
-            <div className="flex items-center px-2 pt-3">
-              <TiLocation className="w-6 h-6" />
-              <p>Guwahati</p>
+            <div className="flex flex-col h-16">
+              <div className="flex items-center px-2 pt-3">
+                <TiLocation className="w-6 h-6" />
+                <p>Guwahati</p>
+              </div>
+              <p className="w-full px-4 mt-1 text-sm text-neutral-600">
+                {doctor.opdDays}
+              </p>
             </div>
-            <p className="w-full px-4 mt-1 text-sm text-neutral-600">
-              {doctor.opdDays}
-            </p>
 
             <div className="flex flex-col h-24 px-4 mt-10">
               <h1 className="text-lg font-medium tracking-wide">
