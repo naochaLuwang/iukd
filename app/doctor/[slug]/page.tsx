7d4cd84a7571813f40cad0b393aadd387f7231e4
Somojit Arambam <somoarambam123@gmail.com>
2023-06-14 21:31:42 +0530

    First Commit

--- app/doctor/[slug]/page.tsx
+++ app/doctor/[slug]/page.tsx
@@ -157,9 +157,9 @@ const DoctorDetails = async ({ params }: any) => {
           <div className="flex justify-center w-full mt-5 h-fit">
             <Link
               href={`/book_an_appoinment?dep=${doctor?.department?.departmentName}&id=${doctor.id}`}
-              className="relative inline-flex items-center px-10 py-3 overflow-hidden text-sm font-medium text-indigo-600 border-2 border-indigo-600 rounded-full hover:text-white group hover:bg-gray-50"
+              className="relative inline-flex items-center px-10 py-3 overflow-hidden text-sm font-medium text-teal-600 border-2 border-teal-600 rounded-full hover:text-white group hover:bg-gray-50"
             >
-              <span className="absolute left-0 block w-full h-0 transition-all bg-indigo-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+              <span className="absolute left-0 block w-full h-0 transition-all bg-teal-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
               <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
                 <svg
                   className="w-5 h-5"
--- components/VerticalTimeline.tsx
+++ components/VerticalTimeline.tsx
@@ -6,56 +6,120 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import Image from "next/image";
+import Link from "next/link";
+import { BiClinic } from "react-icons/bi";
 
 const VerticalTimelineComponent = ({ data }: any) => {
-  console.log(data);
+  const today = new Date().toISOString().split("T")[0]; // Get the current date in YYYY-MM-DD format
 
+  const filteredData = data.filter((item) => item.date >= today);
+  filteredData.sort((a, b) => new Date(a.date) - new Date(b.date));
   return (
     <div>
       <VerticalTimeline>
-        {data &&
-          data.map((item) => (
+        {filteredData &&
+          filteredData.map((item) => (
             <VerticalTimelineElement
               key={item.id}
-              className=" vertical-timeline-element--work"
-              date={item.date}
-              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              className="font-quicksand vertical-timeline-element--work"
+              date={
+                <span className="text-2xl tracking-wider">
+                  {new Date(item.date).toLocaleDateString("en-GB", {
+                    weekday: "long",
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "numeric",
+                  })}
+                </span>
+              }
+              iconStyle={{ background: "teal", color: "#fff" }}
               contentStyle={{
                 background: "white",
                 height: "auto",
-                border: "1px solid black",
+                border: "1px solid teal",
                 borderRadius: "20px",
               }}
               contentArrowStyle={{
-                borderRight: "7px solid  rgb(33, 150, 243)",
+                borderRight: "7px solid  teal",
               }}
+              icon={<BiClinic />}
             >
-              <div className="flex flex-col w-full h-auto ">
+              <div className="flex flex-col w-full h-auto bg-white">
                 <div className="flex w-full h-auto space-x-3">
-                  <div className="relative flex-shrink-0 w-40 h-40">
-                    <Image src="/docplaceholder.jpeg" alt="doc image" fill />
-                  </div>
-                  <div className="flex flex-col h-auto space-y-1">
-                    <h1>
+                  {item.doctor.profileUrl === "" ? (
+                    <div className="relative flex-shrink-0 w-40 h-40">
+                      <Image
+                        src="/docplaceholder.jpeg"
+                        alt="doc image"
+                        fill
+                        className="rounded-lg"
+                      />
+                    </div>
+                  ) : (
+                    <div className="relative flex-shrink-0 w-40 h-40">
+                      <Image
+                        src={item.doctor.profileUrl}
+                        alt="doc image"
+                        fill
+                        className="rounded-lg"
+                      />
+                    </div>
+                  )}
+
+                  <div className="flex flex-col h-auto mt-5 space-y-1">
+                    <h1 className="text-2xl font-medium font-quicksand">
                       {item?.doctor?.salutation} {item.doctor?.firstName}{" "}
                       {item.doctor?.lastName}
                     </h1>
-                    <h1 className="text-xs font-medium">
+                    <h1 className="text-sm font-medium font-quicksand">
                       {item?.doctor.qualification}
                     </h1>
-                    <h1 className="text-sm font-medium">
+                    <h1 className="text-sm font-medium tracking-wide text-indigo-950 font-quicksand">
                       {item?.doctor.designation}
                     </h1>
                   </div>
                 </div>
-                <h1 className="mt-5">Consult Our Specialist</h1>
+                <h1 className="mt-5">
+                  Consult <span className="text-lg">Our Specialist</span>
+                </h1>
                 <div className="grid grid-cols-2 gap-2 mt-5">
                   {item?.opdLists.map((opdlist) => (
-                    <div key={opdlist.id}>
-                      <h1 className="text-sm">{opdlist.clinicName}</h1>
-                      <h1 className="text-sm">{opdlist.location}</h1>
+                    <div
+                      key={opdlist.id}
+                      className="py-3 text-center border rounded-md shadow-sm bg-gradient-to-tr from-teal-700 to-teal-800"
+                    >
+                      <h1 className="text-base font-medium text-white font-poppins">
+                        {opdlist.clinicName}
+                      </h1>
+                      <h1 className="text-sm text-white">{opdlist.location}</h1>
+                      <h1 className="text-white">{opdlist.timing}</h1>
                     </div>
                   ))}
+                </div>
+                <div className="flex justify-center w-full mt-5 h-fit">
+                  <Link
+                    href={`/book_an_appoinment?dep=${item?.department?.departmentName}&id=${item.id}`}
+                    className="relative inline-flex items-center px-10 py-3 overflow-hidden text-sm font-medium text-teal-600 border-2 border-teal-600 rounded-full hover:text-white group hover:bg-gray-50"
+                  >
+                    <span className="absolute left-0 block w-full h-0 transition-all bg-teal-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+                    <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+                      <svg
+                        className="w-5 h-5"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M14 5l7 7m0 0l-7 7m7-7H3"
+                        ></path>
+                      </svg>
+                    </span>
+                    <span className="relative">Book an Appoinment</span>
+                  </Link>
                 </div>
               </div>
             </VerticalTimelineElement>
