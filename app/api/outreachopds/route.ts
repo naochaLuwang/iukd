4bf376bdd3b75de33cdc2ea076c9d86a0d802ecf
Somojit Arambam <somoarambam123@gmail.com>
2023-06-14 15:52:27 +0530

    First Commit

--- app/api/outreachopds/route.ts
+++ app/api/outreachopds/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request) {
   const peoples = await prisma.outreachopds.findMany({
     include: {
       opdLists: true,
-      doctors: true,
+      doctor: true,
     },
   });
 
