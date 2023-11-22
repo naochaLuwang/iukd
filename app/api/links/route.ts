667c973ac22703c9b31ce23ad99e9aef1314a91e
Somojit Arambam <somoarambam123@gmail.com>
2023-11-22 07:29:55 +0530

    Mobile responsive added

--- app/api/links/route.ts
+++ app/api/links/route.ts
@@ -4,6 +4,9 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const links = await prisma.links.findMany({
+    where: {
+      status: "ACTIVE",
+    },
     include: {
       sublinks: true,
     },
