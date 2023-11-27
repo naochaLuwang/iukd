450b08c1302e484760f39c83b43e9a9a012d0377
Somojit Arambam <somoarambam123@gmail.com>
2023-11-27 23:12:19 +0530

    Mobile responsive added

--- app/api/doctor/[slug]/route.ts
+++ app/api/doctor/[slug]/route.ts
@@ -10,7 +10,7 @@ export const revalidate = 1;
 export async function GET(request: Request, { params }: { params: IParams }) {
   const { slug } = params;
   console.log(slug);
-  const doctor = await prisma.people.findFirst({
+  const doctor = await prisma.people.findFirstOrThrow({
     where: {
       slug: slug,
     },
