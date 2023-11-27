6e17860dae36704477cf7159c2c63302fdf45cb5
Somojit Arambam <somoarambam123@gmail.com>
2023-11-27 16:12:48 +0530

    Mobile responsive added

--- app/api/counter/route.ts
+++ app/api/counter/route.ts
@@ -2,6 +2,8 @@ import prisma from "../../../lib/prismadb";
 
 import { NextResponse } from "next/server";
 
+export const revalidate = 1;
+
 export async function GET(request: Request) {
   const counters = await prisma.counters.findMany({
     where: {
