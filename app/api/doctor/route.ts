1a6da7d844566316dfa2785805b48bfd366ac979
Somojit Arambam <somoarambam123@gmail.com>
2023-11-27 15:44:10 +0530

    Mobile responsive added

--- app/api/doctor/route.ts
+++ app/api/doctor/route.ts
@@ -2,6 +2,7 @@ import prisma from "../../../lib/prismadb";
 
 import { NextResponse } from "next/server";
 
+export const revalidate = 1;
 export async function GET(request: Request) {
   const doctors = await prisma.people.findMany({
     include: {
--- app/api/links/[slug]/route.ts
+++ app/api/links/[slug]/route.ts
@@ -5,6 +5,8 @@ interface IParams {
   slug?: string;
 }
 
+export const revalidate = 1;
+
 export async function GET(request: Request, { params }: { params: IParams }) {
   const { slug } = params;
   console.log(slug);
